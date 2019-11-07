aaacd9ca28ec0e590cffe52fd2e53a462dd9f544
xlao <xlao@menard-inc.com>
2019-11-07 08:55:09 -0600

    update

--- src/webgl/View.ts
+++ src/webgl/View.ts
@@ -41,14 +41,14 @@ export default class View {
 		// ----------- playground code here -------------- //
 		const mesh3D: Mesh = new Mesh();
 		// mesh3D is rotated by -90 degrees about the X-axis for proper OrbitControls functionality
-		//mesh3D.rotation.x = -Math.PI / 2;
+		// mesh3D.rotation.x = -Math.PI / 2;
 		this.scene.add(mesh3D);
 
 
 		// test animated mesh from a custom Shape class
 		//this.animatedObjs.push(new Shape(this.scene));
 
-		// shape
+		// shape size and points
 		const size: number = 15;
 		const shapePoints: Vector2[] = [
 			new Vector2(), new Vector2(0,size), new Vector2(size,size*2), new Vector2(size,0)
@@ -56,21 +56,23 @@ export default class View {
 		const shape: Shape2D = new Shape2D(shapePoints);
 		this.scene.add(shape.mesh);
 
-		// shadow
+		// the flipped version of the shape - for visual purpose only
 		const rotatedShape: Shape2D = new Shape2D(shapePoints, 0x00ff00);
 		mesh3D.add(rotatedShape.mesh);
 
-		// wall
+		// create the wall
 		const sideIndex: number = 1;
 		const wallGeo = new BoxGeometry(shape.getSideDistance(sideIndex),5,1);
 		const wall = new Mesh(wallGeo, new MeshBasicMaterial({transparent: true, opacity: .5}));
 
+		// align the wall and put into position to the side
 		ShapeUtils.alignRotationWithDirection(wall, shape.getSideDirection(sideIndex));
 		wall.rotateX(Math.PI / 2);
 		wall.position.copy(shape.getSideCenter(sideIndex));
 
+		// offset the wall from the side
 		const offset: Vector3 = shape.getSideVectorNormal(sideIndex);
-		offset.setLength(1);
+		offset.setLength(2);
 		wall.position.add(offset);
 		mesh3D.add(wall);
 
